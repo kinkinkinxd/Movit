c55bfe9202d4c224500d9590980a1dcdf1edd84c
Kittitouch Ingkasompob <kinkinkinxd@gmail.com>
2023-09-22 01:12:35 +0700

    Initial Commit

---
+++ Movit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movit
+//
+//  Created by กิตติธัช อิงคสมภพ on 22/9/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
